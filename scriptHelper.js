6c5d3069a1f3a38aaf0d8c9c16ac1c8d17acac2c
Ashley Hainline <aetindle@gmail.com>
2023-07-04 11:49:28 -0500

    remove trim function to see if tests pass

--- scriptHelper.js
+++ scriptHelper.js
@@ -16,7 +16,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    if (testInput.trim() === '') {
+    if (testInput === '') {
         return ("Empty");
     }
     const inputNum = Number(testInput);
